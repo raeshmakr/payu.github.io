66524c5aca0697fe64c6307ba135319f0de4cc1e
reshma kr <reshma.xtapps@gmail.com>
2017-06-19 17:48:00 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,17 @@
+
+ <form method="post" action="https://sandbox.gateway.payulatam.com/ppp-web-gateway/">
+  <input name="merchantId"    type="hidden"  value="5792443"   >
+  <input name="accountId"     type="hidden"  value="5792443" >
+  <input name="description"   type="hidden"  value="Test PAYU"  >
+  <input name="referenceCode" type="hidden"  value="TestPayU" >
+  <input name="amount"        type="hidden"  value="3"   >
+  <input name="tax"           type="hidden"  value="0"  >
+  <input name="taxReturnBase" type="hidden"  value="0" >
+  <input name="currency"      type="hidden"  value="USD" >
+<!--   <input name="signature"     type="hidden"  value="ba9ffa71559580175585e45ce70b6c37"  > -->
+  <input name="test"          type="hidden"  value="1" >
+  <input name="buyerEmail"    type="hidden"  value="reshma.xtapps@gmail.com" >
+  <input name="responseUrl"    type="hidden"  value="http://localhost/payumoney/response" >
+   <input name="confirmationUrl"    type="hidden"  value="http://localhost/payumoney/confirmation" >
+  <input name="Submit"        type="submit"  value="Enviar" >
+</form>
